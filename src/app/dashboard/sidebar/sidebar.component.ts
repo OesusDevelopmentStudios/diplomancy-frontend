9449243345f584345ea41f836c6ec17933020eaf
Adam Krizar <adam.krizar@gmail.com>
2026-01-25 16:54:01 +0100

    Implement sidebat search

--- src/app/dashboard/sidebar/sidebar.component.ts
+++ src/app/dashboard/sidebar/sidebar.component.ts
@@ -8,6 +8,7 @@ import { tokenStorageKey } from '../../common/common.data';
 import { decrypt } from '../../common/common.helpers';
 
 import { Friend } from './sidebar.friend';
+import { of } from 'rxjs';
 
 @Component({
     selector: 'app-sidebar',
@@ -19,11 +20,15 @@ import { Friend } from './sidebar.friend';
 export class SidebarComponent implements OnInit
 {
     sidebarOpen: boolean = false;
-    friends: Friend[] = [];
-    enemies: Friend[] = [];
     showBlacklist: boolean = false;
+    matchedFriends: Friend[] = [];
+    matchedEnemies: Friend[] = []
 
     token : String = "";
+
+    private friends: Friend[] = [];
+    private enemies: Friend[] = [];
+    private filter: string = "";
 
     ngOnInit()
     {
@@ -36,52 +41,100 @@ export class SidebarComponent implements OnInit
     toggleSidebar(): void
     {
         this.sidebarOpen = !this.sidebarOpen;
+        this.filterData();
     }
 
-    refresghFriends(): void
+    refreshFriends(): void
     {
         // TODO: Fetch friends status from server, for refresh will just display random dummy data
         for (let i = 0; i < 25; i++)
         {
-            this.friends.push(new Friend('Friend ' + (i + 1), Math.random() < 0.5));
+            this.friends.push(new Friend(i, 'Friend ' + (i + 1), Math.random() < 0.5));
         }
+
+        this.filterData();
     }
 
     removeFriend(id: number): void
     {
         // TODO: Update friendlist in the backend
-        if (this.friends.at(id))
+        if (this.matchedFriends.at(id))
         {
-            this.friends.splice(id, 1);
+            const uniqueId = this.matchedFriends.at(id)?.id;
+            const friendIndex = this.friends.findIndex(friend => friend.id === uniqueId);
+            this.friends.splice(friendIndex, 1);
         }
+
+        this.filterData();
     }
 
     addToBlackList(id: number): void
     {
         // TODO: Update firndlist and blacklist in backend
-        const friend = this.friends.at(id);
+        const friend = this.matchedFriends.at(id);
         if (!friend)
         {
             return;
         }
 
         this.enemies.push(friend);
-        this.friends.splice(id, 1);
+        const uniqueId = this.matchedFriends.at(id)?.id;
+        const friendIndex = this.friends.findIndex(friend => friend.id === uniqueId);
+        this.friends.splice(friendIndex, 1);
+        this.filterData();
     }
 
     removeFromBlacklist(id: number)
     {
-        if (this.enemies.at(id))
+        if (this.matchedEnemies.at(id))
         {
-            this.enemies.splice(id, 1);
+            const uniqueId = this.matchedEnemies.at(id)?.id;
+            const enemyIndex = this.enemies.findIndex(enemy => enemy.id === uniqueId);
+            this.enemies.splice(enemyIndex, 1);
         }
+
+        this.filterData();
     }
 
     getFriendClass(list: Friend[], index: number): string
     {
+        console.error("Getting class for index " + index + " in list of length " + list.length);
         if (list.length == 1) return "single";
         if (index == 0) return "first";
         if (index == list.length - 1) return "last";
+
         return "middle";
+    }
+
+    onSearchInput(text: string): void
+    {
+        this.filter = text.toLowerCase();
+        this.filterData();
+    }
+
+    filterData(): void
+    {
+        console.info("Current mode: " + (this.showBlacklist ? "Blacklist" : "Friendslist"));
+        if (this.filter.length == 0)
+        {
+            this.matchedFriends = this.friends;
+            this.matchedEnemies = this.enemies;
+            return;
+        }
+
+        console.warn("Filtering friends: "  + this.filter);
+        for (const enemy of this.enemies)
+        {
+            console.log(enemy.username.toLocaleLowerCase());
+        }
+
+        this.matchedFriends = this.friends.filter(friend => friend.username.toLowerCase().includes(this.filter));
+        this.matchedEnemies = this.enemies.filter(enemy => enemy.username.toLowerCase().includes(this.filter));
+
+        console.warn("Matched friends: " + this.matchedEnemies.length);
+        for (const enemy of this.matchedEnemies)
+        {
+            console.log(enemy.username);
+        }
     }
 }
--- src/app/dashboard/sidebar/sidebar.friend.ts
+++ src/app/dashboard/sidebar/sidebar.friend.ts
@@ -1,6 +1,7 @@
 export class Friend
 {
     constructor(
+        public id: number,
         public username: string,
         public online: boolean
     ) {}
