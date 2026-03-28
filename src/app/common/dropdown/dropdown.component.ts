fcb0cad04aaa54be6c5a50998c68802c5d3882d7
Adam Krizar <adam.krizar@gmail.com>
2026-03-28 18:08:19 +0100

    Finalize dropdown componenet

--- src/app/common/dropdown/dropdown.component.ts
+++ src/app/common/dropdown/dropdown.component.ts
@@ -1,4 +1,4 @@
-import { Component, input, output } from '@angular/core';
+import { Component, input, OnChanges, output } from '@angular/core';
 
 @Component({
     selector: 'app-dropdown',
@@ -6,11 +6,32 @@ import { Component, input, output } from '@angular/core';
     styleUrl: './dropdown.component.css',
 })
 
-export class DropdownComponent
+export class DropdownComponent implements OnChanges
 {
     items = input.required<Array<string>>();
     default = input<number>(0);
     selected = output<string>();
 
     activeElement: number = this.default();
+    width: string = "100px"
+
+    ngOnChanges(): void
+    {
+        let length : number = 0;
+        for (const item in this.items())
+        {
+            if (this.items()[item].length > length)
+            {
+                length = this.items()[item].length
+            }
+        }
+
+        this.width = (14 * length).toString() + "px"
+    }
+
+    onSelected(index: number): void
+    {
+        this.activeElement = index;
+        this.selected.emit(this.items().at(index)!);
+    }
 }
